ffcc6dc392c16a5126f2f6ba346d13a8adf53e17
oahidalgo <ohgovilla@gmail.com>
2024-02-12 19:52:35 -0600

    Initial Repo

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from '../components/AppNav';
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav></AppNav>
+      <p>App</p>
+    </div>
+  );
+}
+
+export default AppLayout;
